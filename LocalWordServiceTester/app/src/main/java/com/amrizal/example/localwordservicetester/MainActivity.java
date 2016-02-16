abdfe78ffe21ae3f583618ad875cbf45602694f0
Amrizal <amrizal@outlook.com>
2016-02-16 16:43:21 +0800

    Add LocalWordServiceTester

---
+++ LocalWordServiceTester/app/src/main/java/com/amrizal/example/localwordservicetester/MainActivity.java
@@ -0,0 +1,75 @@
+package com.amrizal.example.localwordservicetester;
+
+import android.app.ListActivity;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.IBinder;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends ListActivity {
+
+    private ArrayAdapter<String> adapter;
+    private List<String> wordList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        wordList = new ArrayList<String>();
+        adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, android.R.id.text1,
+                wordList);
+
+        setListAdapter(adapter);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Intent intent= new Intent(this, LocalWordService.class);
+        bindService(intent, mConnection,
+                Context.BIND_AUTO_CREATE);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unbindService(mConnection);
+    }
+
+    private LocalWordService s;
+
+    private ServiceConnection mConnection = new ServiceConnection() {
+
+        public void onServiceConnected(ComponentName className,
+                                       IBinder binder) {
+            LocalWordService.MyBinder b = (LocalWordService.MyBinder) binder;
+            s = b.getService();
+            Toast.makeText(MainActivity.this, "Connected", Toast.LENGTH_SHORT)
+                    .show();
+        }
+
+        public void onServiceDisconnected(ComponentName className) {
+            s = null;
+        }
+    };
+
+    public void onClick(View view) {
+        if (s != null) {
+            Toast.makeText(this, "Number of elements" + s.getWordList().size(),
+                    Toast.LENGTH_SHORT).show();
+            wordList.clear();
+            wordList.addAll(s.getWordList());
+            adapter.notifyDataSetChanged();
+        }
+    }
+}
