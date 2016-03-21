9b8ad164a8f4afb4d833fdf0e9662baf5090899c
Amrizal <amrizal@outlook.com>
2016-03-21 14:53:31 +0800

    Show AlertActivity with screen off/lock and play notification sound

--- ServiceUpdateUI/app/src/main/java/com/amrizal/example/serviceupdateuitester/AlertActivity.java
+++ ServiceUpdateUI/app/src/main/java/com/amrizal/example/serviceupdateuitester/AlertActivity.java
@@ -1,9 +1,14 @@
 package com.amrizal.example.serviceupdateuitester;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.PowerManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -11,15 +16,25 @@ import org.w3c.dom.Text;
 
 public class AlertActivity extends AppCompatActivity {
 
+    PowerManager.WakeLock wakeLock;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alert);
 
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD);
+
+        PowerManager pm = (PowerManager) getApplicationContext().getSystemService(POWER_SERVICE);
+        wakeLock = pm.newWakeLock((PowerManager.SCREEN_BRIGHT_WAKE_LOCK | PowerManager.FULL_WAKE_LOCK |PowerManager.ACQUIRE_CAUSES_WAKEUP), "TAG");
+        wakeLock.acquire();
+
         Button dismiss = (Button)findViewById(R.id.button_dismiss);
         dismiss.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                wakeLock.release();
                 finish();
             }
         });
@@ -34,6 +49,23 @@ public class AlertActivity extends AppCompatActivity {
             String count = "The value is " + intent.getStringExtra(MyService.MYSERVICE_MESSAGE);
             TextView text = (TextView) findViewById(R.id.alert_message);
             text.setText(count);
+
+            //loop through to select which alarm to play
+            Integer[] alarmTypes = {RingtoneManager.TYPE_ALARM, RingtoneManager.TYPE_RINGTONE, RingtoneManager.TYPE_NOTIFICATION};
+            Uri notification = null;
+            for(Integer alarmType:alarmTypes){
+                notification = RingtoneManager.getDefaultUri(alarmType);
+                if(null != notification)
+                    break;
+            }
+
+            MediaPlayer mp = MediaPlayer.create(getApplicationContext(), notification);
+            mp.start();
         }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
     }
 }
--- ServiceUpdateUI/app/src/main/java/com/amrizal/example/serviceupdateuitester/MyService.java
+++ ServiceUpdateUI/app/src/main/java/com/amrizal/example/serviceupdateuitester/MyService.java
@@ -72,7 +72,7 @@ public class MyService extends Service {
         broadcaster.sendBroadcast(intent);
 
 
-        if(counter % 10 == 0){
+        if(counter % 10  == 0){
             final Intent activity = new Intent(this, AlertActivity.class);
             activity.putExtras(intent.getExtras());
             activity.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
