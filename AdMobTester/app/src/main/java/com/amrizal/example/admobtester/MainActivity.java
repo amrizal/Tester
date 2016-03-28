c02131a5d34b4645a8d32a4970006e4e4b040fd3
Amrizal <amrizal@outlook.com>
2016-03-28 11:30:19 +0800

    Add AdMobTester

---
+++ AdMobTester/app/src/main/java/com/amrizal/example/admobtester/MainActivity.java
@@ -0,0 +1,71 @@
+package com.amrizal.example.admobtester;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private AdView mAdView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mAdView = (AdView) findViewById(R.id.adView);
+        mAdView.setAdListener(new AdListener() {
+            @Override
+            public void onAdClosed() {
+                super.onAdClosed();
+            }
+
+            @Override
+            public void onAdFailedToLoad(int errorCode) {
+                super.onAdFailedToLoad(errorCode);
+            }
+
+            @Override
+            public void onAdLeftApplication() {
+                super.onAdLeftApplication();
+            }
+
+            @Override
+            public void onAdLoaded() {
+                super.onAdLoaded();
+            }
+
+            @Override
+            public void onAdOpened() {
+                super.onAdOpened();
+            }
+        });
+        AdRequest adRequest = new AdRequest.Builder()
+                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                .addTestDevice(getResources().getString(R.string.sony_experia_p_tester))
+                .build();
+        mAdView.loadAd(adRequest);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        mAdView.resume();
+    }
+
+    @Override
+    protected void onPause() {
+        mAdView.pause();
+        super.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        mAdView.destroy();
+        super.onDestroy();
+    }
+}
--- ApplicationSingleton/app/src/main/java/com/amrizal/example/applicationsingleton/MainActivity.java
+++ ApplicationSingleton/app/src/main/java/com/amrizal/example/applicationsingleton/MainActivity.java
@@ -14,6 +14,11 @@ import android.view.View;
 import android.widget.CompoundButton;
 import android.widget.Switch;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.List;
 import java.util.SimpleTimeZone;
 
 public class MainActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener {
@@ -34,6 +39,15 @@ public class MainActivity extends AppCompatActivity implements CompoundButton.On
         progDialog.setCancelable(false);
 
         MyApplication mApplication = (MyApplication)getApplicationContext();
+
+        String queryResult = "[{\"name\":\"max\",\"add\":\"\",\"lat\":3.161883,\"lng\":101.720512},{\"name\":\"ATM 1\",\"add\":\"No. 1 ロード SS2/75, Petaling Jaya, 47100 Selangor\",\"lat\":3.160513,\"lng\":101.712367},{\"name\":\"ATM 2\",\"add\":\"No. 2 ロード SS2/75, Petaling Jaya, 47100 Selangor\",\"lat\":3.148853,\"lng\":101.70996},{\"name\":\"ATM 3\",\"add\":\"No. 3 ロード SS2/75, Petaling Jaya, 47100 Selangor\",\"lat\":3.158038,\"lng\":101.712605},{\"name\":\"ATM 4\",\"add\":\"No. 4 ロード SS2/75, Petaling Jaya, 47100 Selangor\",\"lat\":3.148878,\"lng\":101.705809},{\"name\":\"ATM 5\",\"add\":\"No. 5 ロード SS2/75, Petaling Jaya, 47100 Selangor\",\"lat\":3.150649,\"lng\":101.71275},{\"name\":\"ATM 6\",\"add\":\"No. 6 ロード SS2/75, Petaling Jaya, 47100 Selangor\",\"lat\":3.159244,\"lng\":101.719742},{\"name\":\"ATM 7\",\"add\":\"No. 7 ロード SS2/75, Petaling Jaya, 47100 Selangor\",\"lat\":3.152817,\"lng\":101.702775},{\"name\":\"ATM 8\",\"add\":\"No. 8 ロード SS2/75, Petaling Jaya, 47100 Selangor\",\"lat\":3.157779,\"lng\":101.715228},{\"name\":\"ATM 9\",\"add\":\"No. 9 ロード SS2/75, Petaling Jaya, 47100 Selangor\",\"lat\":3.154314,\"lng\":101.719366},{\"name\":\"ATM 10\",\"add\":\"No. 10 ロード SS2/75, Petaling Jaya, 47100 Selangor\",\"lat\":3.145767,\"lng\":101.708119},{\"name\":\"ATM 11\",\"add\":\"No. 11 ロード SS2/75, Petaling Jaya, 47100 Selangor\",\"lat\":3.149007,\"lng\":101.702701},{\"name\":\"ATM 12\",\"add\":\"No. 12 ロード SS2/75, Petaling Jaya, 47100 Selangor\",\"lat\":3.145357,\"lng\":101.717594},{\"name\":\"ATM 13\",\"add\":\"No. 13 ロード SS2/75, Petaling Jaya, 47100 Selangor\",\"lat\":3.152031,\"lng\":101.715908},{\"name\":\"ATM 14\",\"add\":\"No. 14 ロード SS2/75, Petaling Jaya, 47100 Selangor\",\"lat\":3.154123,\"lng\":101.719181},{\"name\":\"ATM 15\",\"add\":\"No. 15 ロード SS2/75, Petaling Jaya, 47100 Selangor\",\"lat\":3.158937,\"lng\":101.703091},{\"name\":\"min\",\"add\":\"\",\"lat\":3.143897,\"lng\":101.702498}]";
+        Gson gson = new Gson();
+        Type type = new TypeToken<List<AtmLocation>>() {}.getType();
+        List<AtmLocation> locList = gson.fromJson(queryResult, type);
+        for (AtmLocation atmLocation:locList){
+            Log.d(TAG, atmLocation.getAtmName() + "," + atmLocation.getAddress() + "," + atmLocation.getLatitude() + "," + atmLocation.getLongitude());
+        }
+
     }
 
     @Override
