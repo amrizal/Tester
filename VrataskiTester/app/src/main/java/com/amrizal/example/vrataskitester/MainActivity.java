e808f0c1b92b2949218fa00ffd8706e88bf0247a
Amrizal <amrizal@outlook.com>
2016-05-24 16:03:09 +0800

    Add query

--- VrataskiTester/app/src/main/java/com/amrizal/example/vrataskitester/MainActivity.java
+++ VrataskiTester/app/src/main/java/com/amrizal/example/vrataskitester/MainActivity.java
@@ -1,5 +1,6 @@
 package com.amrizal.example.vrataskitester;
 
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -8,6 +9,7 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 import java.text.DateFormat;
@@ -34,12 +36,31 @@ public class MainActivity extends AppCompatActivity {
         DateFormat dateTimeInstance = SimpleDateFormat.getDateTimeInstance();
         myRef.push().setValue(new LogEntry("Hello, The time is " + dateTimeInstance.format(Calendar.getInstance().getTime())));
 
-        myRef.addValueEventListener(new ValueEventListener() {
+        long MillisInHour = 60*60*1000;
+        //Query query = myRef.orderByChild("timeStamp").startAt(System.currentTimeMillis()).endAt(System.currentTimeMillis() - MillisInHour);
+        Query query = myRef.orderByChild("timeStamp").endAt(System.currentTimeMillis()).startAt(System.currentTimeMillis() - MillisInHour);
+        query.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 for (DataSnapshot postSnapshot : dataSnapshot.getChildren()) {
                     LogEntry logEntry = postSnapshot.getValue(LogEntry.class);
-                    Log.d(TAG, "Value is: " + logEntry);
+                    Log.d(TAG, "Value is: " + logEntry.getDescription());
+                    //Log.d(TAG, "Value is: " + postSnapshot.getValue());
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+        /*myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot postSnapshot : dataSnapshot.getChildren()) {
+                    LogEntry logEntry = postSnapshot.getValue(LogEntry.class);
+                    Log.d(TAG, "Value is: " + logEntry.getDescription());
                     //Log.d(TAG, "Value is: " + postSnapshot.getValue());
                 }
             }
@@ -47,23 +68,6 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onCancelled(DatabaseError databaseError) {
                 Log.w(TAG, "Failed to read value.", databaseError.toException());
-            }
-        });
-
-        /*// Read from the database
-        myRef.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                // This method is called once with the initial value and again
-                // whenever data at this location is updated.
-                String value = dataSnapshot.getValue(String.class);
-                Log.d(TAG, "Value is: " + value);
-            }
-
-            @Override
-            public void onCancelled(FirebaseError firebaseError) {
-                // Failed to read value
-                Log.w(TAG, "Failed to read value.", firebaseError.toException());
             }
         });*/
     }
