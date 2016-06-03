4afb8473d5bc554f3b9b148768ba01e275adbec7
Amrizal <amrizal@outlook.com>
2016-06-03 16:26:06 +0800

    Add address

--- LocationTester/app/src/main/java/com/amrizal/example/locationtester/MainActivity.java
+++ LocationTester/app/src/main/java/com/amrizal/example/locationtester/MainActivity.java
@@ -1,19 +1,27 @@
 package com.amrizal.example.locationtester;
 
 import android.content.Context;
+import android.content.Intent;
 import android.location.Criteria;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.Handler;
+import android.os.ResultReceiver;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.WindowManager;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
+
 import java.util.Calendar;
 
-public class MainActivity extends AppCompatActivity implements LocationListener {
+public class MainActivity extends AppCompatActivity implements LocationListener, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
     private TextView latituteField;
     private TextView longitudeField;
@@ -21,20 +29,23 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
     private String provider;
     private TextView accuracyField;
     private TextView lastUpdatedField;
-
+    private AddressResultReceiver mResultReceiver;
+    private GoogleApiClient mGoogleApiClient;
+    private Location mLastLocation;
+    private String mAddressOutput;
+    private TextView addressField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        initView();
+        initLocation();
+        buildGoogleApiClient();
+    }
 
-        lastUpdatedField = (TextView) findViewById(R.id.last_updated);
-        latituteField = (TextView) findViewById(R.id.TextView02);
-        longitudeField = (TextView) findViewById(R.id.TextView04);
-        accuracyField = (TextView) findViewById(R.id.TextView06);
-
+    private void initLocation() {
+        mResultReceiver = new AddressResultReceiver(new Handler());
         // Get the location manager
         locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
         // Define the criteria how to select the locatioin provider -> use
@@ -50,6 +61,39 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
         } else {
             latituteField.setText("Location not available");
             longitudeField.setText("Location not available");
+        }
+    }
+
+    protected synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+    }
+
+    private void initView() {
+        setContentView(R.layout.activity_main);
+        lastUpdatedField = (TextView) findViewById(R.id.last_updated);
+        latituteField = (TextView) findViewById(R.id.latitude);
+        longitudeField = (TextView) findViewById(R.id.longitude);
+        accuracyField = (TextView) findViewById(R.id.accuracy);
+        addressField = (TextView) findViewById(R.id.location_address_view);
+
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mGoogleApiClient.isConnected()) {
+            mGoogleApiClient.disconnect();
         }
     }
 
@@ -71,11 +115,32 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
 
     @Override
     public void onLocationChanged(Location location) {
+        mLastLocation = location;
         String mydate = java.text.DateFormat.getDateTimeInstance().format(Calendar.getInstance().getTime());
         lastUpdatedField.setText(mydate);
         accuracyField.setText(String.valueOf(location.getAccuracy()));
         latituteField.setText(String.valueOf(location.getLatitude()));
         longitudeField.setText(String.valueOf(location.getLongitude()));
+    }
+
+    /**
+     * Creates an intent, adds location data to it as an extra, and starts the intent service for
+     * fetching an address.
+     */
+    protected void startIntentService() {
+        // Create an intent for passing to the intent service responsible for fetching the address.
+        Intent intent = new Intent(this, FetchAddressIntentService.class);
+
+        // Pass the result receiver as an extra to the service.
+        intent.putExtra(Constants.RECEIVER, mResultReceiver);
+
+        // Pass the location data as an extra to the service.
+        intent.putExtra(Constants.LOCATION_DATA_EXTRA, mLastLocation);
+
+        // Start the service. If the service isn't already running, it is instantiated and started
+        // (creating a process for it if needed); if it is running then it remains running. The
+        // service kills itself automatically once all intents are processed.
+        startService(intent);
     }
 
     @Override
@@ -93,5 +158,53 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
     public void onProviderDisabled(String provider) {
         Toast.makeText(this, "Disabled provider " + provider,
                 Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        if (mLastLocation != null) {
+            // Determine whether a Geocoder is available.
+            if (!Geocoder.isPresent()) {
+                Toast.makeText(this, R.string.no_geocoder_available, Toast.LENGTH_LONG).show();
+                return;
+            }
+
+            startIntentService();
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
+    }
+
+    /**
+     * Receiver for data sent from FetchAddressIntentService.
+     */
+    class AddressResultReceiver extends ResultReceiver {
+        public AddressResultReceiver(Handler handler) {
+            super(handler);
+        }
+
+        /**
+         *  Receives data sent from FetchAddressIntentService and updates the UI in MainActivity.
+         */
+        @Override
+        protected void onReceiveResult(int resultCode, Bundle resultData) {
+
+            // Display the address string or an error message sent from the intent service.
+            mAddressOutput = resultData.getString(Constants.RESULT_DATA_KEY);
+            displayAddressOutput();
+        }
+    }
+
+    private void displayAddressOutput() {
+        addressField.setText(mAddressOutput);
     }
 }
