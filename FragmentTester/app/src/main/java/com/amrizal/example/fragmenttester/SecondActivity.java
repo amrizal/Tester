80397acab92ca5d8c293911f12c57ab101f68349
Amrizal <amrizal@outlook.com>
2016-10-14 11:52:48 +0800

    Add back-button

--- FragmentTester/app/src/main/java/com/amrizal/example/fragmenttester/SecondActivity.java
+++ FragmentTester/app/src/main/java/com/amrizal/example/fragmenttester/SecondActivity.java
@@ -1,9 +1,12 @@
 package com.amrizal.example.fragmenttester;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Intent;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 
 public class SecondActivity extends AppCompatActivity {
@@ -12,6 +15,22 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        android.support.v7.app.ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            // Respond to the action bar's Up/Home button
+            case android.R.id.home:
+                setResult(Activity.RESULT_CANCELED);
+                finish();
+            default:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     public void onClick(View view) {
