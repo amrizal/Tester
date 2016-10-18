58dbd2f1a18dee32210733183b88a654af1254b3
Amrizal <amrizal@outlook.com>
2016-10-18 17:00:43 +0800

    Deleted. yeah

--- NotificationTester/app/src/main/java/com/amrizal/example/notificationtester/EXTRA.java
+++
@@ -1,9 +0,0 @@
-package com.amrizal.example.notificationtester;
-
-/**
- * Created by amrizal.zainuddin on 27/9/2016.
- */
-public class EXTRA {
-    public static final String CLOUD_TOKEN = "cloud_token";
-    public static final String IS_BACK_BUTTON = "back_button";
-}
