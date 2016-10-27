0b7d340508b5c7fc370b754fcb697a2eaf8c1e15
Amrizal <amrizal@outlook.com>
2016-10-27 16:02:01 +0800

    Add dynamic array

--- CardViewTester/app/src/main/java/com/amrizal/example/cardviewtester/MainActivity.java
+++ CardViewTester/app/src/main/java/com/amrizal/example/cardviewtester/MainActivity.java
@@ -6,6 +6,9 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 
@@ -15,39 +18,81 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView mRecyclerView;
     private LinearLayoutManager mLayoutManager;
     private MyRecyclerViewAdapter mAdapter;
+    private ArrayList<DataObject> dataset;
+    private TextView itemCount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        itemCount = (TextView) findViewById(R.id.item_count);
+
         mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
         mRecyclerView.setHasFixedSize(true);
         mLayoutManager = new LinearLayoutManager(this);
         mRecyclerView.setLayoutManager(mLayoutManager);
-        mAdapter = new MyRecyclerViewAdapter(getDataSet());
-        mRecyclerView.setAdapter(mAdapter);
-    }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        ((MyRecyclerViewAdapter) mAdapter).setOnItemClickListener(new MyRecyclerViewAdapter
-                .MyClickListener() {
+        initDataSet();
+        mAdapter = new MyRecyclerViewAdapter(dataset);
+        mRecyclerView.setAdapter(mAdapter);
+
+        mAdapter.setOnItemClickListener(new MyRecyclerViewAdapter.MyClickListener() {
             @Override
             public void onItemClick(int position, View v) {
                 Log.i(TAG, " Clicked on Item " + position);
             }
         });
+
+        refreshItemCount();
     }
 
-    private ArrayList<DataObject> getDataSet() {
-        ArrayList results = new ArrayList<DataObject>();
-        for (int index = 0; index < 20; index++) {
+    private void refreshItemCount() {
+        itemCount.setText("" + dataset.size() + " item(s)");
+    }
+
+    private void initDataSet() {
+        dataset = new ArrayList<>();
+        for (int index = 0; index < 2; index++) {
             DataObject obj = new DataObject("Some Primary Text " + index,
                     "Secondary " + index);
-            results.add(index, obj);
+            dataset.add(index, obj);
         }
-        return results;
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        /*((MyRecyclerViewAdapter) mAdapter).setOnItemClickListener(new MyRecyclerViewAdapter
+                .MyClickListener() {
+            @Override
+            public void onItemClick(int position, View v) {
+                Log.i(TAG, " Clicked on Item " + position);
+            }
+        });*/
+    }
+
+    public void onAddItem(View view) {
+        if(dataset == null){
+            return;
+        }
+
+        int count = dataset.size();
+        dataset.add(new DataObject("Some Primary Text " + count, "Secondary " + count));
+        mAdapter.notifyDataSetChanged();
+
+        refreshItemCount();
+    }
+
+    public void onRemoveItem(View view) {
+        if(dataset == null || dataset.size() == 0){
+            return;
+        }
+
+        int count = dataset.size();
+        dataset.remove(count-1);
+        mAdapter.notifyDataSetChanged();
+
+        refreshItemCount();
     }
 }
