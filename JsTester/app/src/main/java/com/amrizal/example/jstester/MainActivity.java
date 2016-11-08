adb0deddd73ba670d3ec2ba53e8f25b533ff8aae
Amrizal <amrizal@outlook.com>
2016-11-08 16:22:25 +0800

    Update with android docs example

--- JsTester/app/src/main/java/com/amrizal/example/jstester/MainActivity.java
+++ JsTester/app/src/main/java/com/amrizal/example/jstester/MainActivity.java
@@ -1,13 +1,16 @@
 package com.amrizal.example.jstester;
 
+import android.content.Context;
 import android.content.DialogInterface;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.JavascriptInterface;
 import android.webkit.JsPromptResult;
 import android.webkit.JsResult;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
+import android.widget.Toast;
 
 import org.apache.commons.io.IOUtils;
 
@@ -26,67 +29,23 @@ public class MainActivity extends AppCompatActivity {
         browser = (WebView) findViewById(R.id.WebView1);
         browser.getSettings().setJavaScriptEnabled(true);
         browser.getSettings().setBuiltInZoomControls(true);
-        browser.setWebChromeClient(new MyWebChromeClient());
+        browser.addJavascriptInterface(new WebAppInterface(this), "Android");
+        browser.loadUrl("file:///android_asset/sample_3.html");
 
-        InputStream is = getResources().openRawResource(R.raw.sample_1);
-        String content = null;
-        try {
-            content = IOUtils.toString(is);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        IOUtils.closeQuietly(is); // don't forget to close your streams
-
-        if(content != null){
-            browser.loadData(content, "text/html", "UTF-8");
-        }
     }
 
-    private class MyWebChromeClient extends WebChromeClient {
-        @Override
-        public boolean onJsAlert(WebView view, String url, String message,
-                                 final JsResult result) {
-            new AlertDialog.Builder(MainActivity.this)
-                    .setTitle("JavaScript Alert !")
-                    .setMessage(message)
-                    .setPositiveButton(android.R.string.ok,
-                            new AlertDialog.OnClickListener() {
-                                public void onClick(DialogInterface dialog, int which) {
-                                    // do your stuff here
-                                    result.confirm();
-                                }
-                            }).setCancelable(false).create().show();
-            return true;
+    public class WebAppInterface {
+        Context mContext;
+
+        /** Instantiate the interface and set the context */
+        WebAppInterface(Context c) {
+            mContext = c;
         }
-        @Override
-        public boolean onJsConfirm(WebView view, String url, String message,
-                                   final JsResult result) {
-            new AlertDialog.Builder(MainActivity.this)
-                    .setTitle("JavaScript Confirm Alert !")
-                    .setMessage(message)
-                    .setPositiveButton(android.R.string.ok,
-                            new AlertDialog.OnClickListener() {
-                                public void onClick(DialogInterface dialog, int which) {
-                                    // do your stuff here
-                                    result.confirm();
-                                }
-                            }).setCancelable(false).create().show();
-            return true;
-        }
-        @Override
-        public boolean onJsPrompt(WebView view, String url, String message,
-                                  String defaultValue, final JsPromptResult result) {
-            new AlertDialog.Builder(MainActivity.this)
-                    .setTitle("JavaScript Prompt Alert !")
-                    .setMessage(message)
-                    .setPositiveButton(android.R.string.ok,
-                            new AlertDialog.OnClickListener() {
-                                public void onClick(DialogInterface dialog, int which) {
-                                    // do your stuff here
-                                    result.confirm();
-                                }
-                            }).setCancelable(false).create().show();
-            return true;
+
+        /** Show a toast from the web page */
+        @JavascriptInterface
+        public void showToast(String toast) {
+            Toast.makeText(mContext, toast, Toast.LENGTH_SHORT).show();
         }
     }
 }
