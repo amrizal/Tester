7d321532baaf45d2bc738099f64a03fb3ff8cb45
Amrizal <amrizal@outlook.com>
2016-12-14 16:07:51 +0800

    Add location in service

--- LocationTester/app/src/main/java/com/amrizal/example/locationtester/MainActivity.java
+++ LocationTester/app/src/main/java/com/amrizal/example/locationtester/MainActivity.java
@@ -1,15 +1,27 @@
 package com.amrizal.example.locationtester;
 
+import android.Manifest;
 import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.location.LocationManager;
+import android.os.Build;
 import android.support.v4.content.LocalBroadcastManager;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -18,15 +30,26 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int LOCATION_REQUEST=2905;
+    private static final String[] LOCATION_PERMISSION={
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION
+    };
+    private static final String TAG = MainActivity.class.getSimpleName();
+
     ListView listView;
     List<String> logList = new ArrayList<>();
     private ArrayAdapter<String> adapter;
     private BroadcastReceiver receiver;
+    private LocationManager locationManager;
+    private boolean locationNotEnabled;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 
         receiver = new BroadcastReceiver() {
             @Override
@@ -80,7 +103,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void addLog(String logLine) {
-        SimpleDateFormat dateFormat = new SimpleDateFormat("hh:mm:ss");
+        SimpleDateFormat dateFormat = new SimpleDateFormat("hh:mm:ss.SSS");
         String dateString=dateFormat.format(new Date());
 
         logList.add(0, dateString + " >> " + logLine);
@@ -95,9 +118,80 @@ public class MainActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
 
+        if (!servicesAvailable()) {
+            finish();
+        }
+
+        if(!verifyLocationEnabled()){
+            return;
+        }
+
         IntentFilter intentFilter = new IntentFilter();
         intentFilter.addAction(BROADCAST.LOG_LINE);
         LocalBroadcastManager.getInstance(this).registerReceiver(receiver, intentFilter);
+    }
+
+    private boolean verifyLocationEnabled() {
+        if ( !locationManager.isProviderEnabled( LocationManager.GPS_PROVIDER ) ) {
+            locationNotEnabled = true;
+            buildAlertMessageNoGps();
+            return false;
+        }
+
+        if(!canAccessLocation()) {
+            locationNotEnabled = true;
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(LOCATION_PERMISSION, LOCATION_REQUEST);
+            }
+            return false;
+        }
+
+        return true;
+    }
+
+    private boolean canAccessLocation() {
+        return(hasPermission(Manifest.permission.ACCESS_FINE_LOCATION));
+    }
+
+    private boolean hasPermission(String permission) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            return(PackageManager.PERMISSION_GRANTED==checkSelfPermission(permission));
+        }
+
+        return true;
+    }
+
+    private void buildAlertMessageNoGps() {
+        Log.d(TAG, "buildAlertMessageNoGps");
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(getResources().getString(R.string.gps_disable_query))
+                .setCancelable(false)
+                .setPositiveButton(getResources().getString(R.string.yes), new DialogInterface.OnClickListener() {
+                    public void onClick(@SuppressWarnings("unused") final DialogInterface dialog, @SuppressWarnings("unused") final int id) {
+                        startActivity(new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+                    }
+                })
+                .setNegativeButton(getResources().getString(R.string.no), new DialogInterface.OnClickListener() {
+                    public void onClick(final DialogInterface dialog, @SuppressWarnings("unused") final int id) {
+                        dialog.cancel();
+                        finish();
+                    }
+                });
+        final AlertDialog alert = builder.create();
+        alert.show();
+    }
+
+
+    private boolean servicesAvailable() {
+        int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+
+        if (ConnectionResult.SUCCESS == resultCode) {
+            return true;
+        }
+        else {
+            GooglePlayServicesUtil.getErrorDialog(resultCode, this, 0).show();
+            return false;
+        }
     }
 
     @Override
--- LocationTester/app/src/main/java/com/amrizal/example/locationtester/MyLocationService.java
+++ LocationTester/app/src/main/java/com/amrizal/example/locationtester/MyLocationService.java
@@ -1,11 +1,30 @@
 package com.amrizal.example.locationtester;
 
+import android.Manifest;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Bundle;
 import android.os.IBinder;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.LocalBroadcastManager;
 
-public class MyLocationService extends Service {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+
+public class MyLocationService extends Service implements LocationListener, GoogleApiClient.OnConnectionFailedListener, GoogleApiClient.ConnectionCallbacks {
+
+    private int count = 0;
+
+    private LocationRequest mLocationRequest;
+    private GoogleApiClient mGoogleApiClient;
 
     public MyLocationService() {
     }
@@ -13,11 +32,26 @@ public class MyLocationService extends Service {
     @Override
     public void onCreate() {
         sendMessage("Service::onCreate");
+
+        initLocationRequest();
+        buildGoogleApiClient();
+    }
+
+    protected synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addOnConnectionFailedListener(this)
+                .addConnectionCallbacks(this)
+                .addApi(LocationServices.API)
+                .build();
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         sendMessage("Service::onStartCommand");
+
+        if (!mGoogleApiClient.isConnected())
+            mGoogleApiClient.connect();
+
         return Service.START_STICKY;
     }
 
@@ -29,12 +63,60 @@ public class MyLocationService extends Service {
 
     private void sendMessage(String logLine) {
         Intent intent = new Intent(BROADCAST.LOG_LINE);
-        intent.putExtra(EXTRA.LOG_LINE, logLine);
+        intent.putExtra(EXTRA.LOG_LINE, "[" + (count++) + "] " + logLine);
         LocalBroadcastManager.getInstance(this).sendBroadcast(intent);
     }
 
     @Override
     public void onDestroy() {
         sendMessage("Service::onDestroy");
+        if (mGoogleApiClient.isConnected()) {
+            LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
+            mGoogleApiClient.disconnect();
+        }
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        sendMessage("Lat:" + location.getLatitude() + ", Lon:" + location.getLongitude());
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        sendMessage("Service::onConnectionFailed-"+ connectionResult.getErrorMessage());
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        sendMessage("Service::onConnected");
+
+        Location l = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        if(l != null){
+            onLocationChanged(l);
+        }
+
+        startLocationUpdate();
+    }
+
+    private void startLocationUpdate() {
+        sendMessage("Service::startLocationUpdate");
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+    }
+
+    private void initLocationRequest() {
+        sendMessage("Service::initLocationRequest");
+
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(5000);
+        mLocationRequest.setFastestInterval(2000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        sendMessage("Service::onConnectionSuspended-" + i);
     }
 }
